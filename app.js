4273db74c66dce034e5d2d8d4353b366d6e03e92
chyt93620 <chyt93620@gmail.com>
2025-10-22 15:45:25 +0800

    修改順序、如果空白不搜尋

--- app.js
+++ app.js
@@ -6,43 +6,41 @@ const siteList = document.querySelector(".siteList");
 // 清空列表，初始化
 siteList.innerHTML = "";
 
-const url =
-  "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json";
-
 btn.addEventListener("click", (e) => {
   e.preventDefault(); //讓按按鈕時不會重整網頁
   const Keyword = searchKeyword.value;
   siteList.innerHTML = ""; // 清空列表，讓新的搜尋結果不會跟舊的衝突
 
-  if (Keyword == "") {
-    return;
-  }
+  if (Keyword != "") {
+    const url =
+      "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json";
+    //按下按鈕才取資料
+    fetch(url)
+      .then((resp) => {
+        return resp.json();
+      })
 
-  fetch(url)
-    .then((resp) => {
-      return resp.json();
-    })
+      .then((station) => {
+        const list = station
 
-    .then((station) => {
-      const s1 = station
+          .filter((station) => {
+            return station.ar.includes(Keyword);
+          })
 
-        .filter((station) => {
-          return station.ar.includes(Keyword);
-        })
+          .forEach((station) => {
+            const stationName = station.sna.replace("YouBike2.0_", "");
+            const address = station.ar;
+            const bikes = station.available_rent_bikes;
 
-        .forEach((station) => {
-          const stationName = station.sna.replace("YouBike2.0_", "");
-          const address = station.ar;
-          const bikes = station.available_rent_bikes;
-
-          const li = document.createElement("li");
-          li.className = "list-group-item fs-5";
-          li.innerHTML = `
+            const li = document.createElement("li");
+            li.className = "list-group-item fs-5";
+            li.innerHTML = `
           <i class="fas fa-bicycle"></i>
           ${stationName} (${bikes})<br />
           <small class="text-muted">${address}</small>
         `;
-          siteList.appendChild(li);
-        });
-    });
+            siteList.appendChild(li);
+          });
+      });
+  }
 });
